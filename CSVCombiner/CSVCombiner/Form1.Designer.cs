563a33a4532458593248ef96ea039c16807c766b
JonasVanGool <vangooljonas@gmail.com>
2016-08-12 10:19:41 +0200

    CSVCombiner
    Combine CSF files to one file.

---
+++ CSVCombiner/CSVCombiner/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CSVCombiner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFiles = new System.Windows.Forms.Button();
+            this.lblFilesSelected = new System.Windows.Forms.Label();
+            this.lblTimeStamps = new System.Windows.Forms.Label();
+            this.cbxRealOffset = new System.Windows.Forms.CheckBox();
+            this.tbxTimeOffset = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCombine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectFiles
+            // 
+            this.btnSelectFiles.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFiles.Name = "btnSelectFiles";
+            this.btnSelectFiles.Size = new System.Drawing.Size(135, 23);
+            this.btnSelectFiles.TabIndex = 0;
+            this.btnSelectFiles.Text = "Select files";
+            this.btnSelectFiles.UseVisualStyleBackColor = true;
+            this.btnSelectFiles.Click += new System.EventHandler(this.btnSelectFiles_Click);
+            // 
+            // lblFilesSelected
+            // 
+            this.lblFilesSelected.AutoSize = true;
+            this.lblFilesSelected.Location = new System.Drawing.Point(12, 58);
+            this.lblFilesSelected.Name = "lblFilesSelected";
+            this.lblFilesSelected.Size = new System.Drawing.Size(0, 13);
+            this.lblFilesSelected.TabIndex = 1;
+            // 
+            // lblTimeStamps
+            // 
+            this.lblTimeStamps.AutoSize = true;
+            this.lblTimeStamps.Location = new System.Drawing.Point(12, 91);
+            this.lblTimeStamps.Name = "lblTimeStamps";
+            this.lblTimeStamps.Size = new System.Drawing.Size(0, 13);
+            this.lblTimeStamps.TabIndex = 2;
+            // 
+            // cbxRealOffset
+            // 
+            this.cbxRealOffset.AutoSize = true;
+            this.cbxRealOffset.Location = new System.Drawing.Point(12, 127);
+            this.cbxRealOffset.Name = "cbxRealOffset";
+            this.cbxRealOffset.Size = new System.Drawing.Size(113, 17);
+            this.cbxRealOffset.TabIndex = 3;
+            this.cbxRealOffset.Text = "Use real timeoffset";
+            this.cbxRealOffset.UseVisualStyleBackColor = true;
+            // 
+            // tbxTimeOffset
+            // 
+            this.tbxTimeOffset.Location = new System.Drawing.Point(12, 164);
+            this.tbxTimeOffset.Name = "tbxTimeOffset";
+            this.tbxTimeOffset.Size = new System.Drawing.Size(35, 20);
+            this.tbxTimeOffset.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Time offset [s]";
+            // 
+            // btnCombine
+            // 
+            this.btnCombine.Location = new System.Drawing.Point(12, 206);
+            this.btnCombine.Name = "btnCombine";
+            this.btnCombine.Size = new System.Drawing.Size(135, 23);
+            this.btnCombine.TabIndex = 6;
+            this.btnCombine.Text = "Combine";
+            this.btnCombine.UseVisualStyleBackColor = true;
+            this.btnCombine.Click += new System.EventHandler(this.btnCombine_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(165, 243);
+            this.Controls.Add(this.btnCombine);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxTimeOffset);
+            this.Controls.Add(this.cbxRealOffset);
+            this.Controls.Add(this.lblTimeStamps);
+            this.Controls.Add(this.lblFilesSelected);
+            this.Controls.Add(this.btnSelectFiles);
+            this.Name = "Form1";
+            this.Text = "CSV Combiner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFiles;
+        private System.Windows.Forms.Label lblFilesSelected;
+        private System.Windows.Forms.Label lblTimeStamps;
+        private System.Windows.Forms.CheckBox cbxRealOffset;
+        private System.Windows.Forms.TextBox tbxTimeOffset;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCombine;
+    }
+}
+
---
+++ CSVCombiner/CSVCombiner/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CSVCombiner
+{
+
+    public partial class Form1 : Form
+    {
+        double maxDeltaTime = 2;
+        OpenFileDialog openFileDialog1;
+        SaveFileDialog saveFileDialog1;
+        Dictionary<String, DateTime> fileDictonary;
+        List<KeyValuePair<List<KeyValuePair<String, DateTime>>,DateTime>> sortedList;
+        
+        public Form1()
+        {
+            fileDictonary = new Dictionary<String,DateTime>();
+            sortedList = new List<KeyValuePair<List<KeyValuePair<String, DateTime>>, DateTime>>();
+            InitializeComponent();
+            InitializeOpenFileDialog();
+        }
+
+        private void InitializeOpenFileDialog()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            // Set the file dialog to filter for graphics files.
+            openFileDialog1.Filter = "Files (*.csv;*.CSV;)|*.csv;*.CSV;";
+
+            // Allow the user to select multiple images.
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Title = "My CSV file Browser";
+        }
+
+        private void btnSelectFiles_Click(object sender, EventArgs e)
+        {
+            string[] formats = { "yyyy-MM-dd_HHmmss" };
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                lblFilesSelected.Text = openFileDialog1.FileNames.Length.ToString() + " Files selected";
+                // Read the files
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    try
+                    {
+                        String[] values = System.IO.Path.ChangeExtension(file, null).Split('_');
+                        String timepStamp = values[values.Length-2] + "_" + values[values.Length-1];
+                        DateTime parsedTimeStamp;
+                        DateTime.TryParseExact(timepStamp, formats, null, 
+                                    DateTimeStyles.AllowWhiteSpaces |
+                                    DateTimeStyles.AdjustToUniversal,
+                                    out parsedTimeStamp);
+
+                        fileDictonary.Add(file, parsedTimeStamp);
+                    }
+                    catch (SecurityException ex)
+                    {
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+                List<KeyValuePair<String, DateTime>> myList = fileDictonary.ToList();
+                myList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+                DateTime startOflogging = new DateTime(0);
+                int tempCount = 0;
+                foreach (KeyValuePair<String, DateTime> pair in myList)
+                {
+                    if (/*pair.Value.Subtract(startOflogging).Seconds >= maxDeltaTime*/tempCount == 0 || tempCount%2==0)
+                    {
+                        startOflogging = pair.Value;
+                        sortedList.Add(new KeyValuePair<List<KeyValuePair<String,DateTime>>,DateTime>(new List<KeyValuePair<String,DateTime>>(),startOflogging));
+                    }
+                    sortedList.Last().Key.Add(pair);
+                    tempCount++;
+                }
+                lblTimeStamps.Text = sortedList.Count.ToString() + " Timestamps detected";
+            }
+        }
+
+        private void convertListToCsv(List<KeyValuePair<List<KeyValuePair<String, DateTime>>,DateTime>> input){
+            double timeOffset = 0;
+            double lastTime = 0;
+            double writeTime = 0;
+            //create headers
+            HashSet<String> headers = new HashSet<string>();
+            foreach (KeyValuePair<List<KeyValuePair<String, DateTime>>, DateTime> subList in input)
+            {
+                foreach (KeyValuePair<String, DateTime> timedFiles in subList.Key)
+                {
+                    // Get prefix 
+                    String prefix = timedFiles.Key.Split('\\').Last().Split('_').First();
+                    foreach (String item in File.ReadLines(timedFiles.Key).First().ToString().Split(','))
+                    {
+                        if (!prefix.Contains("ApplLog"))
+                        {
+                            if(item.Contains("Time")){
+                                headers.Add(item.Replace("\\","").Replace("\"",""));
+                            }else{
+                                headers.Add((prefix + "_" + item).Replace("\\","").Replace("\"",""));
+                            }
+                        }         
+                    }
+                }
+            }
+
+            //Create dictonary with headers
+            Dictionary<String, String> values = new Dictionary<String, String>();
+            foreach (String header in headers)
+            {
+                values.Add(header,"0");
+            }
+
+            // Create csv file
+            Stream myStream;
+            saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "CSV files (*.CSV)|*.CSV";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    using (System.IO.StreamWriter file =   new System.IO.StreamWriter(myStream))
+                    {
+                        // Write headers
+                        String headerString = "";
+                        foreach (KeyValuePair<String, String> data in values)
+                        {
+                            headerString += data.Key + ",";
+                        }
+                        file.WriteLine(headerString.Remove(headerString.Length - 1));
+
+                        List<String> tempHeaders = new List<string>();
+                        int currentLine = 1;
+                        Boolean readNextLine = true;
+                        foreach (KeyValuePair<List<KeyValuePair<String, DateTime>>, DateTime> subList in input)
+                        {
+                            // clear values
+                            for (int k = 0; k < values.Count; k++)
+                                values[values.ElementAt(k).Key] = "0";
+
+                            // Create data string
+                            String dataLine = "";
+                            foreach (KeyValuePair<String, String> data in values)
+                            {
+                                if (data.Key == values.First().Key)
+                                {
+                                    dataLine = (writeTime+timeOffset).ToString() + ",";
+                                }
+                                else
+                                {
+                                    dataLine += "0,";
+                                }
+
+                            }
+                            file.WriteLine(dataLine.Remove(dataLine.Length - 1));
+
+                            readNextLine = true;
+                            while (readNextLine)
+                            {
+                                // clear values
+                                for (int k = 0; k < values.Count; k++)
+                                    values[values.ElementAt(k).Key] = "0";
+
+                                foreach (KeyValuePair<String, DateTime> timedFiles in subList.Key)
+                                {
+                                    String prefix = timedFiles.Key.Split('\\').Last().Split('_').First();
+                                    if (prefix.Contains("ApplLog"))
+                                        continue;
+                                    // Create headers
+                                    tempHeaders.Clear();
+                                    if (true)
+                                    {
+                                        foreach (String item in File.ReadLines(timedFiles.Key).First().ToString().Split(','))
+                                        {
+                                            if (item.Contains("Time"))
+                                            {
+                                                tempHeaders.Add(item.Replace("\\", "").Replace("\"", ""));
+                                            }
+                                            else
+                                            {
+                                                tempHeaders.Add((prefix + "_" + item).Replace("\\", "").Replace("\"", ""));
+                                            }
+                                        }
+                                    }
+                                    String actualLine;
+                                    try{
+                                        actualLine = File.ReadLines(timedFiles.Key).ElementAt(currentLine);
+                                        String[] actualValues = actualLine.Split(',');
+                                        for (int i = 0; i < actualValues.Length-1; i++)
+                                        {
+                                            values[tempHeaders[i]] = actualValues[i];
+                                        }
+                                       
+                                    }catch(Exception e){
+                                        timeOffset = 3;
+                                        readNextLine = false;
+                                        currentLine = 1;
+                                        Console.WriteLine(e.Message);
+                                    }                        
+                                }
+                                // Create data string
+                                dataLine = "";
+                                if (readNextLine)
+                                {
+                                    foreach (KeyValuePair<String, String> data in values)
+                                    {
+                                        if (data.Key == values.First().Key)
+                                        {
+                                            writeTime = double.Parse(data.Value) + lastTime + timeOffset;
+                                            dataLine = writeTime.ToString() + ",";
+                                        }
+                                        else
+                                        {
+                                            dataLine += data.Value + ",";
+                                        }
+
+                                    }
+                                    file.WriteLine(dataLine.Remove(dataLine.Length - 1));
+                                }
+                                else
+                                {
+                                    foreach (KeyValuePair<String, String> data in values)
+                                    {
+                                        if (data.Key == values.First().Key)
+                                        {
+                                            dataLine = writeTime.ToString() + ",";
+                                        }
+                                        else
+                                        {
+                                            dataLine += "0,";
+                                        }
+
+                                    }
+                                    file.WriteLine(dataLine.Remove(dataLine.Length - 1));
+                                }
+                                currentLine++;
+                            }
+                            
+                            lastTime = writeTime;
+                        }
+                    }  
+                    myStream.Close();
+                }
+            }
+
+        }
+
+        private void btnCombine_Click(object sender, EventArgs e)
+        {
+            convertListToCsv(sortedList);
+        }
+
+    }
+}
